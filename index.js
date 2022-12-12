0ebbf07876f5c933f4a66733ca313e8b3a5f8d9d
DiprodipDas <diprodip755@gmail.com>
2022-12-12 11:52:40 +0600

    second commit

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ app.use(express.json());
 
 
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
     res.send('my portfolio is running successfully')
 })
 
-app.get('/project', (req, res) => {
+app.get('/project',(req, res) => {
     res.send(project);
 })
 
-app.get('/project/:id', (req, res) => {
+app.get('/project/:id',(req, res) => {
     const id = req.params.id;
     const selectedProject = project.find(proj => proj._id === id);
     res.send(selectedProject);
 })
 
-app.listen(port, () => console.log(`my portfolio is successfully running on port ${port}`))
+app.listen(port, () => console.log('my portfolio is successfully running on port:',port))
